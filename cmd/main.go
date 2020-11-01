f5df47e5895fe615b0de956f9e660443e35053be
Murali Bashyam <mbcoder@gmail.com>
2020-11-01 17:55:31 +0000

    Modifications to allow create, read, update and delete of storagepool CRD's.

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 
+	crdv1 "github.com/murali-bashyam/crdexample/pkg/apis/crd.example.com/v1"
 	crdclientset "github.com/murali-bashyam/crdexample/pkg/client/clientset/versioned"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -15,6 +16,44 @@ import (
 
 func PrintPod(pod *apiv1.Pod) {
 	fmt.Println("Pod Name ", pod.Name, " Host IP ", pod.Status.HostIP, " IP ", pod.Status.PodIP)
+}
+
+func createOrUpdatePool(c *crdclientset.Clientset, poolname string, quota int, domain string) error {
+
+	pool, err := c.CrdV1().StoragePools("default").Get(context.TODO(), poolname, metav1.GetOptions{})
+
+	if err != nil {
+		pool := &crdv1.StoragePool{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: poolname,
+			},
+			PoolSpec: crdv1.StoragePoolSpec{
+				FailureDomain: domain,
+				Quota:         quota,
+			},
+		}
+		_, err = c.CrdV1().StoragePools("default").Create(context.TODO(), pool, metav1.CreateOptions{})
+		if err == nil {
+			fmt.Printf("Storage pool created %s \n", poolname)
+		}
+	} else {
+		pool.PoolSpec.Quota = quota
+		pool.PoolSpec.FailureDomain = domain
+		_, err = c.CrdV1().StoragePools("default").Update(context.TODO(), pool, metav1.UpdateOptions{})
+		if err == nil {
+			fmt.Printf("Storage pool updated %s \n", poolname)
+		}
+	}
+
+	return err
+}
+
+func deletePool(c *crdclientset.Clientset, poolname string) error {
+	err := c.CrdV1().StoragePools("default").Delete(context.TODO(), poolname, metav1.DeleteOptions{})
+	if err == nil {
+		fmt.Printf("Storage pool deleted %s \n", poolname)
+	}
+	return err
 }
 
 func main() {
@@ -38,6 +77,16 @@ func main() {
 		log.Fatal("Failed to initialize CRD clientset", err)
 	}
 
+	err = createOrUpdatePool(crdclient, "blockpool", 1024, "rack")
+	if err != nil {
+		log.Fatal("Failed to create storage pool ", err)
+	}
+
+	err = createOrUpdatePool(crdclient, "bpool", 512, "host")
+	if err != nil {
+		log.Fatal("Failed to create storage pool ", err)
+	}
+
 	pool_list, err := crdclient.CrdV1().StoragePools("default").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		log.Fatal("Error listing all storage pool: %v", err)
@@ -46,6 +95,11 @@ func main() {
 	for _, pool := range pool_list.Items {
 		fmt.Printf("Pool %s with quota %d and failureDomain %s \n", pool.Name,
 			pool.PoolSpec.Quota, pool.PoolSpec.FailureDomain)
+	}
+
+	err = deletePool(crdclient, "bpool")
+	if err != nil {
+		log.Fatal("Failed to delete storage pool ", err)
 	}
 
 	pod, err := clientset.CoreV1().Pods("rook-ceph").Get(context.TODO(), "rook-discover-w5vz5", metav1.GetOptions{})
